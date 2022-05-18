1b013b97615af5ee17e398b600555b3b2bc227e8
ymcui <me@ymcui.com>
2022-05-18 15:10:26 +0800

    update

--- build.sh
+++
@@ -1,9 +0,0 @@
-bundle exec 'jekyll build'
-#cp -r _site/assets _site/task
-#cp -r _site/assets _site/dates
-#cp -r _site/assets _site/submission
-#cp -r _site/assets _site/leaderboard
-#cp -r _site/assets _site/program
-#cp -r _site/assets _site/committee
-rm _site.zip
-zip -q -r _site.zip _site
